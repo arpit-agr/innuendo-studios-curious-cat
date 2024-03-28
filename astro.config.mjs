15ef01ac1484cfa880c99f6e33e797f0210f58de
arpit-agr <arpit.agrawal2502@gmail.com>
2024-03-28 17:02:26 +0530

    Allow writing nested CSS declarations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), partytown(), sitemap()]
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+			nesting: true,
+		}),
+		partytown(),
+		sitemap(),
+	],
 });
