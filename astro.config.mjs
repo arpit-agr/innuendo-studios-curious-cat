80e84d5d31afdb411633af08484ad5797b451331
arpit-agr <arpit.agrawal2502@gmail.com>
2024-10-03 11:58:35 +0530

    update astro, install astro-font and astro-icon, remove open props and postcss-jit-props

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
@@ -10,6 +12,7 @@ export default defineConfig({
 			applyBaseStyles: false,
 			nesting: true
 		}),
-		sitemap()
+		sitemap(),
+		icon()
 	]
 });
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,5 @@
-const postcssJitProps = require('postcss-jit-props');
-const OpenProps = require('open-props');
-
 module.exports = {
 	plugins: [
-		postcssJitProps(OpenProps),
 		require('tailwindcss/nesting'),
 		require('tailwindcss'),
 		require('postcss-custom-media')
