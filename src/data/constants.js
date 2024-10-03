5b9b0c0092541a657781ddb0891ecdb827d01be3
arpit-agr <arpit.agrawal2502@gmail.com>
2024-10-03 12:11:31 +0530

    add constants

---
+++ src/data/constants.js
@@ -0,0 +1,8 @@
+export const USER_NAME = 'InnuendoStudios';
+export const SITE_TITLE = 'Innuendo Studios';
+export const youtubeUrl =
+	'https://www.youtube.com/channel/UC5fdssPqmmGhkhsJi4VcckA';
+export const twitterUrl = 'https://x.com/InnuendoStudios';
+export const tumblrUrl = 'https://innuendostudios.tumblr.com/';
+export const patreonUrl = 'https://www.patreon.com/innuendostudios/about';
+export const curiousCatUrl = 'https://curiouscat.live/InnuendoStudios';
