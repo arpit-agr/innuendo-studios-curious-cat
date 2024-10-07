838118f81d90d32f11868d03913fc4b6b0a12465
arpit-agr <arpit.agrawal2502@gmail.com>
2024-10-07 08:19:59 +0530

    install and configure pagefind to run after astro:build:done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
 import icon from 'astro-icon';
+import {spawn} from 'node:child_process';
+import {fileURLToPath} from 'node:url';
+import {dirname, relative} from 'node:path';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +15,25 @@ export default defineConfig({
 			nesting: true
 		}),
 		sitemap(),
-		icon()
+		icon(),
+		// Custom integration for Pagefind using the astro:build:done hook
+		{
+			name: 'pagefind-integration',
+			hooks: {
+				'astro:build:done': async ({dir}) => {
+					const targetDir = fileURLToPath(dir);
+					const cwd = dirname(fileURLToPath(import.meta.url));
+					const relativeDir = relative(cwd, targetDir);
+
+					return new Promise(resolve => {
+						spawn('npx', ['-y', 'pagefind', '--site', relativeDir], {
+							stdio: 'inherit',
+							shell: true,
+							cwd
+						}).on('close', () => resolve());
+					});
+				}
+			}
+		}
 	]
 });
