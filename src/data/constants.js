f1da8eed20506deb6c26bc6d372cb1776d5469cb
arpit-agr <arpit.agrawal2502@gmail.com>
2024-11-14 09:04:17 +0530

    add bluesky link

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,9 @@ export const ANSWER_COUNT = answers.length;
 export const SITE_TITLE = 'Innuendo Studios';
 export const youtubeUrl =
 	'https://www.youtube.com/channel/UC5fdssPqmmGhkhsJi4VcckA';
+export const blueskyUrl =
+	'https://bsky.app/profile/innuendostudios.bsky.social';
 export const twitterUrl = 'https://x.com/InnuendoStudios';
 export const tumblrUrl = 'https://innuendostudios.tumblr.com/';
-export const patreonUrl = 'https://www.patreon.com/innuendostudios/about';
+export const patreonUrl = 'https://www.patreon.com/innuendostudios/';
 export const curiousCatUrl = 'https://curiouscat.live/InnuendoStudios';
